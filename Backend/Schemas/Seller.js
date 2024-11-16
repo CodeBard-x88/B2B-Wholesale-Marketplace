b2fa88b610c0b289609ce531bb0ed750222bb5b9
Muhammad Tayyab <141912893+CodeBard-x88@users.noreply.github.com>
2024-11-17 02:20:15 +0500

    feat<fixes in seller route and seller model>

--- Backend/Schemas/Seller.js
+++ Backend/Schemas/Seller.js
@@ -3,11 +3,10 @@ const db = require("../config/db");
 const SellerSchema = db.Schema({
     BusinessEmail: {type: String, required: true, unique: true},
     AssociatedBuyerAccountEmail: {type: String, required: true},
-    Name: {type: String, required: true},
     CNIC: {type: String, required: true},
     NTN: {type: String, required: true},
     IBAN : {type: String, required: true},
-    sellerAccountStatus : {type: String, enum: ['pending', 'approved' , 'rejected'], default: 'pending'}
+    sellerAccountStatus : {type: String, enum: ['pending', 'approved' , 'rejected', 'temporarilyBlocked'], default: 'pending'}
 })
 
 const SellerModel = db.model("Sellers" , SellerSchema);
