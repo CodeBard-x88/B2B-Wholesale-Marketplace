b701635a289c758c6942fa089dce05d6a6dfd8eb
Muhammad Tayyab <141912893+CodeBard-x88@users.noreply.github.com>
2024-11-22 01:25:23 +0500

    feat<Recreate login page to make it responsive>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 // src/App.jsx
 import React from "react";
 import Header from "./Components/Header";
-import HeroImage from "./Components/HeroImage";
 import LoginForm from "./Components/LoginForm";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-black text-green-500 flex flex-col">
+    <div className="min-h-screen bg-black  text-green-500 flex flex-col">
       <Header />
       {/* Responsive Layout */}
       <div className="flex flex-col md:flex-row items-center justify-center p-8 space-y-8 md:space-y-0 md:space-x-8">
-        <HeroImage />
         <LoginForm />
       </div>
     </div>
--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,18 +1,16 @@
-// src/components/Header.jsx
 import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-black text-green-500">
+    <header className="flex justify-between items-center px-14 my-6 bg-black text-green-500">
       <div className="flex items-center">
         <img
-          src="KaroobarLogo.png" // Path relative to public folder
+          src="KaroobarLogo.png" 
           alt="KAROOBAR Logo"
-          className="h-10"
+          className="h-16"
         />
       </div>
-      {/* Responsive Navigation */}
-      <nav className="hidden md:flex space-x-6 text-lg">
+      <nav className="hidden md:flex space-x-10 text-lg items-center">
         <a href="www.google.com" className="hover:text-green-300">Home</a>
         <a href="www.google.com" className="hover:text-green-300">Contact</a>
         <a href="www.google.com" className="hover:text-green-300">About</a>
